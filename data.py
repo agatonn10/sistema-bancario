85e365328b692ac04790a724a9776cd9ee73bc4a
agatonn10 <222233712+agatonn10@users.noreply.github.com>
2025-08-20 07:22:23 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,17 @@
+import json
 
+ARCHIVO_DATOS = "base_datos.json"
+Datos = {}
+
+def guardado(data):
+  """Datos Guardandose al JSON"""
+  with open(ARCHIVO_DATOS, "w", encoding = "utf-8") as f:
+    json.dump(data,f, indent=4, ensure_ascii=False)
+
+def cargar_datos():
+  """Datos Guardados al archivo JSON"""
+  try:
+    with open(ARCHIVO_DATOS, "r", encoding="utf-8") as f:
+      return json.load(f)
+  except FileNotFoundError:
+    return {}
